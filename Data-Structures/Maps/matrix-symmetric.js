8adc01ab0d974595e657ab645cace6fae395b8b8
Mohamed Sakr <mohamedisakr@gmail.com>
2019-07-23 10:45:20 +0200

    Update matrix-symmetric.js

--- Data-Structures/Maps/matrix-symmetric.js
+++ Data-Structures/Maps/matrix-symmetric.js
@@ -1,15 +1,13 @@
-
 function isSymmetric(matrix) {
-    let isOk = true;
+    //let isOk = true;
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
           if(i !== j && matrix[i][j] !== matrix[j][i]) {
-              isOk = false;
-              break;
+              return false;
           }
         }        
     }
-    return isOk;
+    return true;
 }
 
 let matrix = [
