de8c380b9961bfbdf12affe8cab7efdab9908a7e
Mohamed Sakr <mohamedisakr@gmail.com>
2019-08-21 08:40:13 +0200

    Update 04-largest-difference.js

--- Algorithms/04-largest-difference.js
+++ Algorithms/04-largest-difference.js
@@ -1,4 +1,5 @@
 function getLargestDifference(a) {
+    if (a.length === 0) { throw new Error('Array is empty'); }
     let max = a[1] - a[0];
     for (let i = 2; i < a.length; i++) {
         let diff = (Math.abs(a[i] - a[i - 1]));
