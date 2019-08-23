69a90467405a6f943f68f35ded9ff05136798b26
Mohamed Sakr <mohamedisakr@gmail.com>
2019-08-23 14:23:07 +0200

    Update 29-mode.js

--- Algorithms/29-mode.js
+++ Algorithms/29-mode.js
@@ -1,53 +1,22 @@
-// this function as Rosen BUT does not work
-function findMode(a) {
+function getMode(a) {
     let n = a.length;
-    let modecount = 0;
-    let i = 0;
-    let value = 0;
+    let mode = 0;
     let count = 0;
-
-    while (i <= n) {
-        value = a[i];
-        count = 1;
-        while (i <= n && a[i] === value) {
+    let prev = 0;
+    let largest = 0;
+    for (let i = 0; i < n; i++) {
+        if (a[i] === a[i + 1]) {
+            mode = a[i];
             count++;
-            i++;
+        }
+        else {
+            if (count > largest) {
+                largest = count;
+                prev = mode
+            }
+            mode = 0;
+            count = 0;
         }
     }
-
-    let mode = 0;
-    if (count > modecount) {
-        modecount = count;
-        mode = value;
-    }
-
-    return mode;
+    return prev;
 }
-
-let v = [3, 3, 5, 6, 6, 6, 9, 9]; //  [1, 10, 2, 3, 4, 7, 7, 7, 9, 9]
-let mode = findMode(v);
-console.log(mode);
-v.sort();
-
-/* // find mode using JavaScript object
-
-// let a = [6, 3, 9, 6, 6, 5, 9, 3];
-let a = [1, 7, 7, 7, 9, 2, 3, 10, 9, 4];
-let occur = {};
-for (let i = 0; i < a.length; i++) {
-    let j = a[i];
-    occur[j] = occur[j] ? occur[j] + 1 : 1;
-}
-
-let max = 0;
-for (const key in occur) {
-    if (occur.hasOwnProperty(key)) {
-        const value = occur[key];
-        if (max < value) { max = value; }
-    }
-}
-
-const key = Object.keys(occur).find(key => occur[key] === max);
-let mode = parseInt(key); //Number(m);
-console.log(mode);
-*/
