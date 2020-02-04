f326f2d165aafa0e3e71336970eae6350c421e56
Mohamed Sakr <mohamedisakr@gmail.com>
2020-02-04 09:27:22 +0200

    Update helper-functions.js

--- JavaScript-Examples/helper-functions.js
+++ JavaScript-Examples/helper-functions.js
@@ -1,3 +1,4 @@
+// Simulate Null Coalescing Operator, for checking if the value has null or undefined
 function checkForNullOrUndefined(val){
     if(typeof val!=='undefined'|| val!==null){
         return val;
